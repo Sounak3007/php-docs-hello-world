e922c990a8b6033c151c7a5b85818e15a30975c7
Sounak3007 <118169528+Sounak3007@users.noreply.github.com>
2023-01-05 17:55:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -188,7 +188,7 @@ catch(Exception $e){
    $DOB = $_POST['DOB'];
          $date = date("Y/m/d H:i:s");
          // Insert data
-         $sql_insert = "INSERT INTO registration_tbl (name, Address,Gender,phone,email,Course,DOB,date)
+         $sql_insert = "INSERT INTO registration_table (name, Address,Gender,phone,email,Course,DOB,date)
                     VALUES (?,?,?,?,?,?,?,?)";
          $stmt = $conn->prepare($sql_insert);
          $stmt->bindValue(1, $name);
